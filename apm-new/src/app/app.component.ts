e26893ca60e72cf11c8d601c444a7068e66febb6
Kalluri_Hemant <40833064+Hemantkalluri20@users.noreply.github.com>
2025-02-02 09:27:47 +0530

    Css changes

--- apm-new/src/app/app.component.ts
+++ apm-new/src/app/app.component.ts
@@ -3,27 +3,16 @@ import { ProductListComponent } from "../products/product-list.component";
 
 @Component({
   selector: 'pm-root',
-  template: `
-    <div><h1>{{pageTitle}}</h1>
-      <pm-products></pm-products>
-    </div>
-  `,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  //template: `
+  //<div><h1>{{pageTitle}}</h1>
+  //<pm-products></pm-products>
+  //</div>
+  //`,
   imports: [ProductListComponent]
 })
 
 export class AppComponent {
-  pageTitle: string = 'Acme Product Management';
+  pageTitle: string = 'Product Management Portal';
 }
-
-// import { Component } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
-
-// @Component({
-//   selector: 'pm-root',
-//   imports: [RouterOutlet],
-//   templateUrl: './app.component.html',
-//   styleUrl: './app.component.css'
-// })
-// export class AppComponent {
-//   title = 'apm-new';
-// }
