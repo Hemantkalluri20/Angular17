016bfa37bb32e9da606f86a0b0324b34006c996c
Kalluri_Hemant <40833064+Hemantkalluri20@users.noreply.github.com>
2025-03-20 23:30:09 +0530

    Services and http request using observable

--- apm-new/src/app/app.module.ts
+++ apm-new/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from '../products/product-list.component';
 import { FormsModule } from '@angular/forms';
-import { convertToSpacesPipe } from '../shared/convert-to-space.pipe';
+import { convertToSpacesPipe } from '../shared/pipes/convert-to-space.pipe';
 
 @NgModule({
   declarations: [],
--- apm-new/src/products/product-list.component.ts
+++ apm-new/src/products/product-list.component.ts
@@ -2,12 +2,14 @@ import { CommonModule } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
 import { FormsModule } from '@angular/forms';
 import { IProduct } from "./products";
-import { ConvertToSpacesPipe } from "../shared/convert-to-space.pipe";
+import { ConvertToSpacesPipe } from "../shared/pipes/convert-to-space.pipe";
+import { ProductService } from "./product.service";
 
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
+  providers:[ProductService],
   imports: [FormsModule, CommonModule, ConvertToSpacesPipe]
 
 })
@@ -16,9 +18,16 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   filteredProducts: IProduct[] = [];
-
+  errorMessage: string = '';
+  sub: any;
 
   private _listFilter: string = '';
+  private _productService;
+
+  constructor(private productService: ProductService) {
+    this._productService= productService;
+
+  }
 
   get listFilter(): string {
     return this._listFilter;
@@ -31,65 +40,25 @@ export class ProductListComponent implements OnInit {
 
   showImage: boolean = false;
   products: IProduct[] = [
-    {
-      "productId": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2021",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "/src/assets/images/leaf_rake.png"
-    },
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2021",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "assets/images/garden_cart.png"
-    },
-    {
-      "productId": 5,
-      "productName": "Hammer",
-      "productCode": "TBX-0048",
-      "releaseDate": "May 21, 2021",
-      "description": "Curved claw steel hammer",
-      "price": 8.9,
-      "starRating": 4.8,
-      "imageUrl": "assets/images/hammer.png"
-    },
-    {
-      "productId": 8,
-      "productName": "Saw",
-      "productCode": "TBX-0022",
-      "releaseDate": "May 15, 2021",
-      "description": "15-inch steel blade hand saw",
-      "price": 11.55,
-      "starRating": 3.7,
-      "imageUrl": "assets/images/saw.png"
-    },
-    {
-      "productId": 10,
-      "productName": "Video Game Controller",
-      "productCode": "GMG-0042",
-      "releaseDate": "October 15, 2020",
-      "description": "Standard two-button video game controller",
-      "price": 35.95,
-      "starRating": 4.6,
-      "imageUrl": "assets/images/xbox-controller.png"
-    }
   ];
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
   ngOnInit(): void {
     console.log('Productlist oninit')
-    this._listFilter = 'cart'
+     this.sub = this._productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.performFilter(this.listFilter);
+      },
+      error: err => this.errorMessage = err
+    });
+    // this._listFilter = 'cart'
   }
   
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
   performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase();
     return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().includes(filterBy));
